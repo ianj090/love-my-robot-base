9b9f5c0a6db5c17ad8249edc3f50f61a552fb89d
Ian <ianjenatz@ufm.edu>
2019-11-07 11:27:45 -0600

    Added basic python file creation function as a test

--- lex/lex.py
+++ lex/lex.py
@@ -3,6 +3,10 @@ import requests, sys
 
 app = Flask(__name__)
 
+f = open("transpiled/test.py", "w+")
+for i in range(10):
+    f.write("Test Line %d\r\n" % (i+1))
+f.close()
 
 @app.route("/")
 def translation():

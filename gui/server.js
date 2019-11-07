d74718e52389ff583f0471cbf8c60b7e8d7cae35
Ian <ianjenatz@ufm.edu>
2019-11-07 09:33:08 -0600

    Renamed file

--- gui/server.js
+++ gui/server.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const pug = require('pug');
+var bodyParser = require('body-parser');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello From Express'))
+app.set("view engine", "pug")
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let commands = ["hello", "yay"]
+
+app.get('/', (req, res) => res.render('gui', {commands}))
+
+app.post('/save-command', function(req, res) {
+    console.log(req.body);
+    commands.push(req.body.command);
+    res.json({message:"New command added"})
+})
+app.post('/delete-command', function(req, res) {
+    console.log(req.body);
+    commands = commands.filter(e => e !== req.body.command);
+    res.json({message:"Command deleted"})
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

ab4e8bb5d509e935bd789f272978300682003ee7
Ian <ianjenatz@ufm.edu>
2019-11-12 08:10:56 -0600

    Attempt to create a link between the node and python servers

--- gui/server.js
+++ gui/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const pug = require('pug');
 var bodyParser = require('body-parser');
+var request = require('request-promise');
 const app = express()
 const port = 8080
 
@@ -12,7 +13,37 @@ app.use(bodyParser.json());
 
 let commands = ["hello", "yay"]
 
-app.get('/', (req, res) => res.render('gui', {commands}))
+
+app.get('/', (req, res) => res.render('gui', {
+    commands // Specify the data with data: "foo" (commands: "algo")
+    
+}));
+
+app.get('/postdatatoFlask', async function (req, res) {
+    var data = { // this variable contains the data you want to send
+        data1: "foo",
+        data2: "bar"
+    }
+
+    var options = {
+        method: 'POST',
+        uri: 'http://127.0.0.1:8080/postdata',
+        body: data,
+        json: true // Automatically stringifies the body to JSON
+    };
+    
+    var returndata;
+    var sendrequest = await request(options)
+    .then(function (parsedBody) {
+        console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
+        returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+    
+    res.send(returndata);
+});
 
 app.post('/save-command', function(req, res) {
     console.log(req.body);

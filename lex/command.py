b495a10f10af96160a58135abea8ee98948b1c6f
DavidCorzo <davidcorzo@ufm.edu>
2019-11-20 15:39:44 -0600

    inicio de comunicacion con cozmo

---
+++ lex/command.py
@@ -0,0 +1,13 @@
+import cozmo
+
+
+def interpret(D):
+    for k, v in D.items():
+        if k == 'action':
+            pass
+        elif k == 'text':
+            pass
+
+
+D = {"action": "say", "text": "hello"}
+interpret(D)
--- lex/lex.py
+++ lex/lex.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
-import requests, sys
+import requests
+import sys
 
 app = Flask(__name__)
 
-get = requests.get('http://127.0.0.1:8080/getdata') # GET request
+get = requests.get('http://127.0.0.1:8080/getdata')  # GET request
 data = get.json()
 # process this JSON data and do something with it
 print(data)
@@ -13,9 +14,10 @@ if data != "":
     result = {"status": "SUCCESSFUL"}
 else:
     result = {"status": "FAILED"}
- 
+
 # now immediately sending a post request with new data
-post = requests.post('http://127.0.0.1:8080/postdata', json=result) # the POST request
+post = requests.post('http://127.0.0.1:8080/postdata',
+                     json=result)  # the POST request
 print(post.text)
 print(result)
 
@@ -24,15 +26,18 @@ for i in range(10):
     f.write("Test Line %d\r\n" % (i+1))
 f.close()
 
+
 @app.route("/")
 def translation():
     return render_template(
         "translated.html",
     )
 
+
 @app.route("/lex")
 def lex():
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

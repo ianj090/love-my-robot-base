d971b2c5ed0e6f44128d17427ad3a7cc2efd8ed3
Ian <ianjenatz@ufm.edu>
2019-11-19 22:45:05 -0600

    Added add command and delete command to page

--- gui/public/js/example.js
+++ gui/public/js/example.js
@@ -1,15 +1,16 @@
+// YA FUNCIONA
 $(document).ready(function() {
-    $('.send').click(function() {
-        let newCommand = $('#action').val()
+    $('.addbutton').click(function() {
+        let newCommand = $(this).closest('#action').clone().children().remove().end().text()
         console.log(newCommand);
         if (newCommand && newCommand.length > 0) {
             console.log('yeah')
             $.post('/save-command', {command:newCommand}, function(data, status) {
                 console.log(`${data.message} and status is ${status}`)
-                alert(data.message)
+                // alert(data.message)
                 setTimeout(function() {
                     location.reload();
-                }, 500);
+                }, 100);
             })
         }
         else {
@@ -18,15 +19,43 @@ $(document).ready(function() {
     })
 })
 
+// YA FUNCIONA
 $(document).ready(function() {
-    $('.delete').click(function() {
+    $('.deletebutton').click(function() {
         let someCommand = $(this).closest('#target').clone().children().remove().end().text()
-        let Command = someCommand.replace(" ", "")
+        let Command = someCommand//.replace(" ", "")
         console.log(Command);
         $(this).closest('#target').remove()
         $.post('/delete-command', {command:Command}, function(data, status) {
             console.log(`${data.message} and status is ${status}`)
-            alert(data.message)
+            // alert(data.message)
         })
     })
 })
+
+// TODAVIA NO ESTA TERMINADO
+$(document).ready(function() {
+    $('.clearbutton').click(function() {
+        let Commands = $(".target").text()
+        $(".target").empty();
+        console.log("Commands")
+        $.post('/delete-all', {command:Commands}, function(data, status) {
+            console.log(`${data.message} and status is ${status}`)
+            // alert(data.message)
+        })
+    })
+})
+
+// $(document).ready(function() {
+//     $('.clearbutton').click(function() {
+//         $("#target").each(function(i) {
+//             console.log(`$(this).closest('#target')`)
+//             $(this).closest('#target').remove()
+//             $.post('/delete-command', {command:Command}, function(data, status) {
+//                 console.log(`${data.message} and status is ${status}`)
+//                 // alert(data.message)
+//             })
+//         })
+//     })
+// })
+

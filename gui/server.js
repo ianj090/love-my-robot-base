123895be283fade30b73e14e1756557e000ef5fd
DavidCorzo <davidcorzo@ufm.edu>
2019-11-21 11:14:50 -0600

    2019-11-21 quitar metodo de boton para texto

--- gui/server.js
+++ gui/server.js
@@ -15,53 +15,53 @@ let commands = []
 let texts = []
 
 app.get('/postdata', async function (req, res) {
-    var data = {commands, texts}
- 
+    var data = { commands, texts }
+
     var options = {
         method: 'POST',
         uri: 'http://0.0.0.0:5000/lex',
         body: data,
         json: true // Automatically stringifies the body to JSON
     };
-    
+
     var returndata;
     var sendrequest = await request(options)
-    .then(function (parsedBody) {
-        console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
-        returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-    
+        .then(function (parsedBody) {
+            console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
+            returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
     res.send(returndata);
-    
+
 });
 
-app.get('/', (req, res) => res.render('gui', {commands, texts}))   
+app.get('/', (req, res) => res.render('gui', { commands, texts }))
 
 
-app.post('/save-command', function(req, res) {
+app.post('/save-command', function (req, res) {
     console.log(req.body);
     commands.push(req.body.command);
-    res.json({message:"New command added"})
+    res.json({ message: "New command added" })
 })
-app.post('/delete-command', function(req, res) {
+app.post('/delete-command', function (req, res) {
     console.log(req.body);
     commands = commands.filter(e => e !== req.body.command);
-    res.json({message:"Command deleted"})
+    res.json({ message: "Command deleted" })
 })
-app.post('/delete-all', function(req, res) {
+app.post('/delete-all', function (req, res) {
     console.log(req.body);
     commands = []
-    res.json({message:"Commands deleted"})
+    res.json({ message: "Commands deleted" })
 })
 
-app.post('/save-text', function(req, res) {
-    console.log(req.body);
-    texts.push(req.body.text);
-    res.json({message:"Text added"})
-})
+// app.post('/save-text', function(req, res) {
+//     console.log(req.body);
+//     texts.push(req.body.text);
+//     res.json({message:"Text added"})
+// })
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))

15f82a7ab56b4f35d01e5e98bfdb8269cbc96347
DavidCorzo <davidcorzo@ufm.edu>
2019-11-21 11:18:41 -0600

    2019-11-21 Primer test de datos funcional

--- lex/command.py
+++ lex/command.py
@@ -3,11 +3,16 @@ import cozmo
 
 def interpret(D):
     for k, v in D.items():
-        if v[0] == 'SAY':
-            print(D)
-        if v[1] == 'LIGHT':
-            print(D)
+        for command in v:
+            if command == 'SAY':
+                cozmo.run_program(cozmo_program)
+            if command == 'LIGHT':
+                print(D)
     # print(D)
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.say_text("Hello World").wait_for_completed()
 
 
 # D = {"action": "say", "text": "hello"}

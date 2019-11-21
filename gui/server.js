5040622a5e8b91463bed85d00bfb6ce07e7062fa
DavidCorzo <davidcorzo@ufm.edu>
2019-11-20 21:58:24 -0600

    2019-11-20 separacion de ul para el clear

--- gui/server.js
+++ gui/server.js
@@ -12,77 +12,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let commands = []
+let text =["HI", "RED"]
 
 app.get('/postdata', async function (req, res) {
-    var data = { // this variable contains the data you want to send
-        data1: "yup",
-        data2: "bar"
-    }
-
+    var data = {commands, text}
+ 
     var options = {
         method: 'POST',
         uri: 'http://0.0.0.0:5000/lex',
         body: data,
         json: true // Automatically stringifies the body to JSON
     };
-
+    
     var returndata;
     var sendrequest = await request(options)
-        .then(function (parsedBody) {
-            console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
-            returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-
+    .then(function (parsedBody) {
+        console.log(parsedBody); // parsedBody contains the data sent back from the Flask server
+        returndata = parsedBody; // do something with this data, here I'm assigning it to a variable.
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+    
     res.send(returndata);
+    
 });
 
-app.get('/', (req, res) => res.render('gui', { commands }))
-
-// app.post("/postdata", (req, res) => {
-//     var code = req.body.status;
-//     console.log(code);
-//     res.send("Successful");
-// }); // Esta parte no es muy importante en nuestro caso (solo lo puse por si acaso)
+app.get('/', (req, res) => res.render('gui', {commands}))   
 
 
-// app.get("/getdata", (req, res) => {
-//     var data = {};
-//     res.status(200).json(data)
-// });
-
-// app.post("/send_data", function(req, res) {
-//     app.get("/getdata", (req, res) => {
-//         var data = { // this is the data you're sending back during the GET request
-//             action: "say",
-//             text: "hello world"
-//         }
-//         res.status(200).json(data)
-//     });
-// })
-
-
-
-app.post('/save-command', function (req, res) {
+app.post('/save-command', function(req, res) {
     console.log(req.body);
     commands.push(req.body.command);
-    res.json({ message: "New command added" })
+    res.json({message:"New command added"})
 })
-app.post('/delete-command', function (req, res) {
+app.post('/delete-command', function(req, res) {
     console.log(req.body);
     commands = commands.filter(e => e !== req.body.command);
-    res.json({ message: "Command deleted" })
+    res.json({message:"Command deleted"})
 })
-app.post('/delete-all', function (req, res) {
+app.post('/delete-all', function(req, res) {
     console.log(req.body);
-    let some = req.body + '';
-    var block = some.split(" ");
-    for (item in block) {
-        commands = commands.filter(e => e !== req.body.command);
-    }
-    res.json({ message: "Commands deleted" })
+    commands = []
+    res.json({message:"Commands deleted"})
 })
 
 
